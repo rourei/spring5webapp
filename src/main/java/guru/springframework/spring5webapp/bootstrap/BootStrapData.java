7e58943ba3bcbe0b1bfbc9335a14bd5a3faaa76b
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-09 08:32:12 +0200

    [Ex2] included new Publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +18,19 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override // needs to be implemented (-> CommandLineRunner)
     public void run(String... args) throws Exception{
+
+        System.out.println("Started in Bootstrap.");
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Development", "123123");
@@ -43,8 +50,14 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
-        System.out.println("Started in Bootstrap.");
+        Publisher oreilly = new Publisher("O'Reilly", "1005 Gravenstein Highway North", "Sebastopol",
+                "California", "95472");
+
+        publisherRepository.save(oreilly);
+
         System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println(oreilly.toString());
 
     }
 }
