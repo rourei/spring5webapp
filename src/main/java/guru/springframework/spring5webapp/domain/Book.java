2ec00bdfb901b781ddf90b4048eec302154fc3ad
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-09 07:58:19 +0200

    [L17] removed set members from constructor

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity // defining JPA entity
@@ -16,16 +17,15 @@ public class Book {
     @ManyToMany // describes the relationship between the POJOs / JPA entities
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
     inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>(); // initialize as empty set
 
     // JPA requires zero argument constructor
     public Book() {
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
     }
 
     public Long getId() {
