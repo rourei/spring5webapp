b1244fba571bcaf073d65a06ff7233c4d8a147e1
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-11 06:58:56 +0200

    [L18] implemented new relationships and rearranged the code

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -32,32 +32,46 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap.");
 
+        // Create new publisher instance
+        Publisher oreilly = new Publisher("O'Reilly", "1005 Gravenstein Highway North", "Sebastopol",
+                "California", "95472");
+
+        // Save current state to the database
+        publisherRepository.save(oreilly);
+
+        // Create new author and book instance and couple them
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Development", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        // Couple the book with the publisher instance
+        ddd.setPublisher(oreilly); // Obviously not the 'correct' publisher
+        oreilly.getBooks().add(ddd);
 
         // method chaining, add() is member of Set type -> retrieved member is immediately modified
 
+        // Save current state to the database
         authorRepository.save(eric); // Spring uses Hibernate to save to an in-memory H2 database
         bookRepository.save(ddd);
+        publisherRepository.save(oreilly);
 
+        // Create new author and book instance and couple them
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EEE Development without EJB", "234234");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        // Couple the book with the publisher instance
+        noEJB.setPublisher(oreilly);
+        oreilly.getBooks().add(noEJB);
 
+        // Save current state to the database
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher oreilly = new Publisher("O'Reilly", "1005 Gravenstein Highway North", "Sebastopol",
-                "California", "95472");
-
         publisherRepository.save(oreilly);
 
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Number of publishers: " + publisherRepository.count());
-        System.out.println(oreilly.toString());
+        System.out.println("Publisher number of books: " + oreilly.getBooks().size());
 
     }
 }
